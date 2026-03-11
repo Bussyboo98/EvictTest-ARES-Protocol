af26b45ae4a048357daa3d77d65d502d3a17b7c8
Bussyboo98 <ogunburebusayo.j@gmail.com>
2026-03-11 10:43:47 +0100

    pull

--- Ares-Foundation/script/DeployAresTreasury.s.sol
+++ Ares-Foundation/script/DeployAresTreasury.s.sol
@@ -8,11 +8,15 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployAresTreasury is Script {
     function run() external {
-        /* Retrieve deployment configurations from environment variables
-         If not set, use default values for local testing or placeholder*/
+        /*
+         If not set, use default values for local testing or placeholder
+        */
+         // sst yourDEPLOYER_ADDRESS
         address deployer = vm.envOr("DEPLOYER_ADDRESS", msg.sender);
+        // set your EMERGENCY_GUARDIAN
         address guardian = vm.envOr("EMERGENCY_GUARDIAN", deployer);
-        address rewardToken = vm.envOr("REWARD_TOKEN", address(0));
+        // set your REWARD_TOKEN 
+        address rewardToken = vm.envOr("REWARD_TOKEN ", address(0));
         
        
         address[] memory initialGovernors = new address[](1);
@@ -20,7 +24,7 @@ contract DeployAresTreasury is Script {
 
         vm.startBroadcast(deployer);
 
-        // 1. Deploy AresTreasury
+        //  Deploy AresTreasury
         AresTreasury treasury = new AresTreasury(initialGovernors, guardian);
         console.log("AresTreasury deployed at:", address(treasury));
 
@@ -31,7 +35,7 @@ contract DeployAresTreasury is Script {
             );
             console.log("RewardDistributor deployed at:", address(distributor));
 
-            // 3. Link RewardDistributor to Treasury
+            // Link RewardDistributor to Treasury
             treasury.setRewardDistributor(address(distributor));
             console.log("RewardDistributor linked to AresTreasury");
         } else {
